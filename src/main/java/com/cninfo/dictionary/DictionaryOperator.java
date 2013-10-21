00cfbccb74529d7634e59b0123ec1a70897cc310
bulletlu <121577822@qq.com>
2013-10-21 15:53:34 +0800

    增加zkservlet

---
+++ src/main/java/com/cninfo/dictionary/DictionaryOperator.java
@@ -0,0 +1,10 @@
+package com.cninfo.dictionary;
+
+public interface DictionaryOperator {
+	
+	public void loadWords();
+	
+	public void addWords();
+	
+	public void removeWords();
+}
---
+++ src/main/java/com/cninfo/dictionary/impl/ZKDictionaryOperator.java
@@ -0,0 +1,25 @@
+package com.cninfo.dictionary.impl;
+
+import com.cninfo.dictionary.DictionaryOperator;
+
+public class ZKDictionaryOperator implements DictionaryOperator {
+
+	@Override
+	public void loadWords() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addWords() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void removeWords() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
